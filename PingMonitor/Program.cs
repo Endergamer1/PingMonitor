bdd0f5e974d119d7bae02f637d2ed9aefcec4447
jan.focke <jan.focke@W10P-WS-13>
2023-03-08 09:57:18 +0100

    Projektdateien hinzufügen.

---
+++ PingMonitor/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Net.NetworkInformation;
+using System.IO;
+using System.Text;
+using System.Runtime.InteropServices;
+
+[DllImport("kernel32.dll", SetLastError = true)]
+static extern bool AllocConsole();
+
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+const int SW_HIDE = 0;
+const int SW_SHOW = 5;
+
+static void ShowConsoleWindow()
+{
+    var handle = GetConsoleWindow();
+
+    if (handle == IntPtr.Zero)
+    {
+        AllocConsole();
+    }
+    else
+    {
+        ShowWindow(handle, SW_SHOW);
+    }
+}
+
+static void HideConsoleWindow()
+{
+    var handle = GetConsoleWindow();
+    ShowWindow(handle, SW_HIDE);
+}
+
+string FileLocation = @"C:\Service\Ping\PingLog.txt";
+
+
+Ping p = new Ping();
+PingReply r;
+Console.WriteLine("Bitte Ping Addresse eingeben: ");
+string PingAddress = Console.ReadLine();
+StringBuilder sb = new StringBuilder("", 100);
+HideConsoleWindow();
+while (true)
+{
+    r = p.Send(PingAddress);
+    if (r.Status == IPStatus.Success)
+    {
+    }
+    else if (r.Status == IPStatus.TimedOut || r.Status == IPStatus.DestinationNetworkUnreachable)
+    {
+        using (StreamWriter sw = File.AppendText(FileLocation))
+        {
+            Console.WriteLine("Boop");
+            sw.WriteLine("Fehler ab: " + DateTime.Now);
+            while (true)
+            {
+                r = p.Send(PingAddress);
+                if (r.Status == IPStatus.Success)
+                {
+                    Console.WriteLine("Boop");
+                    sw.WriteLine("Erfolgreich ab: " + DateTime.Now);
+                    break;
+                }
+            }
+
+        }
+    }
+}
